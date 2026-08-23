1c9c593f4906a7740b589f334f980f2a2edd1aca
preetpalsinghkanda <preetpalsinghkanda@gmail.com>
2026-08-23 11:09:28 +0530

    fixed hero curve img

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <div className="min-h-screen w-full bg-[hsl(218,28%,13%)]">
+    <div className="min-h-screen w-full bg-[hsl(219,30%,18%)]">
       <div className=" max-w-7xl w-full mx-auto">
         <Navbar />
         <Hero/>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import illustration1 from "../assets/illustration-intro.png";
+import curve from "../assets/bg-curvy-desktop.svg";
 
 const Hero = () => {
   return (
-    <div className="border flex flex-col gap-10">
+    <div className=" flex min-h-screen flex-col gap-10">
       <div className="flex flex-col items-center justify-center gap-12">
         <img src={illustration1} alt="" />
-        <h5 className="text-4xl font-[600] text-center text-white">
+        <h5 className="text-4xl z-10 font-[600] text-center text-white">
           All your files in one secure location,
           <span className="block"> accessible anywhere.</span>
         </h5>
       </div>
-      <div className="flex flex-col gap-8 justify-center">
-        <p className="text-[22px] text-white text-center ">
+      <div className="flex flex-col gap-8 mb-46 justify-center">
+        <p className="text-[22px] z-10 text-white text-center ">
           <span className="block">
             Fylo stores all your most important files in one secure location.
           </span>
@@ -21,7 +22,14 @@ const Hero = () => {
           </span>{" "}
           friends family, and co-workers.
         </p>
-        <button className="bg-[hsl(198,60%,50%)] self-center text-[20px] text-white font-[500] rounded-full px-20 py-2">Get Started</button>
+        <button className="bg-[hsl(198,60%,50%)] z-10 self-center text-[20px] text-white font-[500] rounded-full px-20 py-2">
+          Get Started
+        </button>
+        <img
+          src={curve}
+          className="absolute  w-full left-1/2 translate-y-1/7 -translate-x-1/2 "
+          alt=""
+        />
       </div>
     </div>
   );
